99e8ecbba206e5459fde6eaa5a01575f86af3b44
朱 俊健 <wisejoker@shutekiMacBook-Pro.local>
2013-10-07 13:53:12 +0800

    Initial Commit

---
+++ PhotoForBingyanV0-1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PhotoForBingyanV0-1
+//
+//  Created by 朱 俊健 on 13-10-7.
+//  Copyright (c) 2013年 朱 俊健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
